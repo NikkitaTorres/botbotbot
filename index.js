f0312795ea5eb1d79780972bbaf2326ba9646b2c
NikkitaTorres <nikkitatorres@yahoo.com>
2024-03-06 12:46:20 -0800

    fixed issue with results not appearing in terminal

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ const puppeteer = require("puppeteer");
   });
   const page = await browser.newPage();
   await page.goto(
-    "https://www.amazon.com/s?rh=n%3A15391321011&fs=true&ref=lp_15391321011_sar"
+    "https://www.amazon.com/s?i=electronics&rh=n%3A15391321011&fs=true&page=2&qid=1709757775&ref=sr_pg_2"
   );
 
   const productsHandles = await page.$$(
-    "div.s.-main-slot.s-result-list.s-search-results.sg-row > .s-result-item"
+    "div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item"
   );
 
-  for (const producthandle of productsHandles){
+  for (const producthandle of productsHandles) {
     let title = "Null";
     let price = "Null";
     let img = "Null";
 
     try {
-    title = await page.evaluate(
-      (el) => el.querySelector("h2 > a > span").textContent, 
-      producthandle
-    );
+      title = await page.evaluate(
+        (el) => el.querySelector("h2 > a > span").textContent,
+        producthandle
+      );
     } catch (error) {}
 
     try {
       price = await page.evaluate(
-        (el) => el.querySelector(".a-price > .a-offscreen").textContent, 
+        (el) => el.querySelector(".a-price > .a-offscreen").textContent,
         producthandle
       );
     } catch (error) {}
 
     try {
-      title = await page.evaluate(
-        (el) => el.querySelector(".s-image").getAttribute("src"), 
+      img = await page.evaluate(
+        (el) => el.querySelector(".s-image").getAttribute("src"),
         producthandle
       );
     } catch (error) {}
 
-    console.log(title, price, img)
+    console.log(title, price, img);
   }
   //await browser.close();
 })();
