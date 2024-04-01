bcfb47f6de34fdbadef92a599ee0759e7ddd2205
Nikkita Torres <nikkitatorres@yahoo.com>
2024-03-31 19:45:16 -0700

    create a new test file

---
+++ class.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless:false});
+  const page = await browser.newPage();
+  await page.goto('https://www.amazon.com/s?i=electronics&rh=n%3A15391321011&fs=true&page=91', {
+    waitUntil: "load"
+  });
+
+  const is_disabled = await page.$('.s-pagination-disabled') !== null;
+
+  console.log(is_disabled)
+
+  await browser.close();
+})();
